7d2514bfa0599d383bc4b4129d3abfff5e464917
Zandraa <zandraa.perjesi@gmail.com>
2018-06-04 22:21:47 +0200

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better.

---
+++ src/main/kotlin/com/zandraa/giflandinfinite/gif/scraper/GifScraperApplication.kt
@@ -0,0 +1,11 @@
+package com.zandraa.giflandinfinite.gif.scraper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GifScraperApplication
+
+fun main(args: Array<String>) {
+    runApplication<GifScraperApplication>(*args)
+}
---
+++ src/main/kotlin/com/zandraa/giflandinfinite/gif/scraper/service/ScraperService.kt
@@ -0,0 +1,33 @@
+package com.zandraa.giflandinfinite.gif.scraper.service
+
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.springframework.stereotype.Service
+
+@Service
+class ScraperService {
+    fun getPage(): String{
+        var gifUrl = ""
+        try{
+            val connection: Connection = Jsoup.connect("http://gifland.us")
+            connection.userAgent("Mozilla/5.0")
+            val document: Document = connection.get()
+            val image: Element = document.select("img[alt=Super gif]")[0]
+            gifUrl = image.attr("src")
+        }
+        catch (ex: Exception){
+            print(ex)
+        }
+        return gifUrl
+    }
+
+    fun getUrls(): List<String>{
+        var gifUrls = mutableListOf<String>()
+        for (i in 1..5) {
+            gifUrls.add(getPage())
+        }
+        return gifUrls
+    }
+}
